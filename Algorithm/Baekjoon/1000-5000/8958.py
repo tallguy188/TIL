1de7887ccd6f1964281ee70ebb7ffd99536ba82c
tallguy188 <cksghd4220@naver.com>
2022-06-24 23:16:52 +0900

    Algorithm - Today's Problem

---
+++ Algorithm/Baekjoon/1000-5000/8958.py
@@ -0,0 +1,16 @@
+"""
+OOXXOXXOOO와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+OOXXOXXOOO의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+"""
+
+n = int(input())
+characters = []
+sentence = input()
+
+for char in sentence:
+	characters.append(char)
+	
+print(characters)
