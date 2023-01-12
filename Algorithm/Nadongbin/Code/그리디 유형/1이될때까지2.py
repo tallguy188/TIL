8fcc2b203d3a72b0c736467d3c79f3d1f50b1c59
tallguy188 <cksghd4220@naver.com>
2023-01-12 16:43:06 +0900

    Today'a Algorithm

---
+++ Algorithm/Nadongbin/Code/그리디 유형/1이될때까지2.py
@@ -0,0 +1,26 @@
+n,k = map(int,input().split())
+
+count = 0
+
+
+while n>=k:
+
+
+    while n%k!=0:
+        n = n-1
+        count+=1
+        
+    
+    n = n//k
+        
+    count += 1
+
+
+while n>1:
+    n = n-1
+    count+=1
+    
+
+print(count)
+
+    
