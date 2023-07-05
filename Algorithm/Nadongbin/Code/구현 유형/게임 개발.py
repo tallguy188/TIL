04452bb8e30e1fcc0209d61b0e761832640ea63b
tallguy188 <cksghd4220@naver.com>
2023-07-05 17:00:40 +0900

    Today's Algorithm

--- Algorithm/Nadongbin/Code/구현 유형/게임 개발.py
+++ Algorithm/Nadongbin/Code/구현 유형/게임 개발.py
@@ -1,66 +1,68 @@
-# n,m을 입력받음
+n, m = map(int,input().split())
+x,y , direction = map(int,input().split())
 
-n,m = map(int,input().split())
 
-#방문한 위치를 저장하기 위한 맵을 생성, 0으로 초기화
+#방문한 위치를 저장하기 위한 맵
+d = [[0]*m for _ in range(n)]
 
-d = [[0] *m for _ in range(n)]
-
-#현재 x좌표, y좌표, 방향 입력받기
-x,y, direction = map(int,input().split())
-
-#전체 맵 정보 입력
+d[x][y] = 1
 
 array = []
 
+# 맵의 정보를 입력받는다.
 for i in range(n):
     array.append(list(map(int,input().split())))
 
-
-# 북,동,남,서 방향정의
+#북동남서
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
-#회전함수
 
 def turn_left():
     global direction
-    direction -1 = direction
+    direction -=1
     if direction == -1:
         direction = 3
 
-
-
 count = 1
-turn_time= 0
-
+turn_time = 0 # 방향을 다돌았을때를 가정
 while True:
-    turn_left() #왼쪽으로 회전
+
+    turn_left()
+
     nx = x + dx[direction]
+
     ny = y + dy[direction]
 
-    if d[nx][ny]== 0 and array[nx][ny] == 0:
-        d[nx][ny] ==1
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
         x = nx
         y = ny
-        count += 1
-        turn_time=0
-        continue # 이동한 다음 다시 조건을 따져봐야되므로 continue
-
-    else :       # 회전한 이후 정면이 가본칸이거나 바다인경우
+        count +=1
+        turn_time = 0
+        continue
+    else:
         turn_time +=1
 
-    if turn_time == 4:   #네방향 모두 갈수없는 경우
-        nx = x-dx[direction]
-        ny = y-dy[direction]
-        #뒤로갈수있으면 이동
-        if array[nx][ny] ==0:
-            x =nx
+    if turn_time == 4:
+        nx = x -dx[direction]
+        ny=  y -dy[direction]
+
+        if array[nx][ny] == 0:
+            x = nx
             y = ny
 
-        #뒤가 바다인경우
         else:
             break
         turn_time = 0
 
 print(count)
+
+
+
+
+
+
+
+
+
--- Algorithm/Nadongbin/Code/구현 유형/왕실의 나이트(다시).py
+++ Algorithm/Nadongbin/Code/구현 유형/왕실의 나이트(다시).py
@@ -1,18 +1,19 @@
-# 왕실의 나이트 (다시)
 
+a = input()
 
-input_data=  input()
+row = int(a[1])
 
-row  = int(input_data[1])
+column = int(ord(a[0])) - ord('a') + 1
 
-column = int(ord(input_data[0])) - int(ord('a')) +1
-
-
-steps = [(-2,-1),(-1,-2),(1,-2),(2,-1)]
-
-
+nexts = [(-2,1),(-2,-1),(2,1),(2,-1),(1,-2),(1,2),(-1,-2),(-1,2)]
 
 result = 0
+for next in nexts:
+    next_row = row + next[0]
+    next_column = column + next[1]
 
-for step in steps:
+    if next_row > 0 and next_row < 9  and next_column >0 and next_column <9:
+        result +=1
 
+
+print(result)
